50852ff9f1d4fea6fbebb383678c7c3ac399ad19
141fa04022 <dinesh.vignan4@gmail.com>
2017-02-10 10:32:02 +0530

    Create tes.php

---
+++ tes.php
@@ -0,0 +1,56 @@
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "test";
+$name = $_POST['name'];
+// $uname = $_POST['uname'];
+// $age = $_POST['age'];
+// $pswd = $_POST['pswd'];
+
+
+
+
+// Create connection
+$conn = mysqli_connect($servername, $username, $password, $dbname);
+// Check connection
+if (!$conn) {
+    die("Connection failed: " . mysqli_connect_error());
+}
+
+$sql = "select * from log where name = '$name'";
+// $sql="insert into log(name,uname,age,pswd)
+// values ('$name','$uname','$age','$pswd')";
+$result = mysqli_query($conn, $sql);
+// if (mysqli_num_rows($result)>0) {
+// 	echo "inserted";
+// 	# code...
+// }
+
+if (mysqli_num_rows($result) > 0) {
+      while($row = mysqli_fetch_assoc($result)) {
+      	$name = $row["name"];
+      	$uname=$row["uname"];
+
+//        //echo "<form><input type="text" name="name" value=$row["uname"]/><form/>" ."uname" ."<br/>";
+
+    }
+} else {
+    echo "0 results";
+}
+
+mysqli_close($conn);
+?>
+<html>
+<body>
+<div class="container">
+
+<form>
+Details:<br>
+<div class="row">
+Name:<input type="text" name="name" value="<?php echo $name ?>" />
+Username:<input type="text" name="name" value="<?php echo $uname ?>" />
+</div>
+</form>
+</body>
+</html>
